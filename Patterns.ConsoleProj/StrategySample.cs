d2056175766cede38c62a48eb35a6bf5627a186c
mi.osipov <mi.osipov@tinkoff.ru>
2021-11-17 23:13:59 +0300

    add StrategySample

---
+++ Patterns.ConsoleProj/StrategySample.cs
@@ -0,0 +1,38 @@
+﻿namespace Patterns.ConsoleProj
+{
+    public interface ICreditCartBillingStrategy
+    {
+        double GetCheck(int monthDuration);
+    }
+    
+    public class PremiumCreditCartBillingStrategy : ICreditCartBillingStrategy
+    {
+        public double GetCheck(int monthDuration)
+        {
+            return 3.5 * monthDuration;
+        }
+    }
+    
+    public class CommonCreditCartBillingStrategy : ICreditCartBillingStrategy
+    {
+        public double GetCheck(int monthDuration)
+        {
+            return 5 * monthDuration;
+        }
+    }
+
+    public class CreditCard
+    {
+        private readonly ICreditCartBillingStrategy _cartBilling;
+        private int _montCounter;
+        
+        public CreditCard(ICreditCartBillingStrategy cartBilling)
+        {
+            _cartBilling = cartBilling;
+        }
+
+        public void SpentMonth() => _montCounter++;
+
+        public double GetCheck() => _cartBilling.GetCheck(_montCounter);
+    }
+}
---
+++ Tests/UnitTest/StrategyTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Patterns.ConsoleProj;
+
+namespace UnitTest
+{
+    public class StrategyTest
+    {
+        [Test]
+        public void Two_Strategies_One_Env()
+        {
+            var prem = new PremiumCreditCartBillingStrategy();
+            var common = new CommonCreditCartBillingStrategy();
+            
+            var premCard = new CreditCard(prem);
+            var commonCard = new CreditCard(common);
+            
+            premCard.SpentMonth();
+            commonCard.SpentMonth();
+            
+            Assert.AreNotEqual(premCard.GetCheck(), commonCard.GetCheck());
+            Assert.AreEqual(premCard.GetCheck(), 3.5);
+            Assert.AreEqual(commonCard.GetCheck(), 5);
+        }
+    }
+}
